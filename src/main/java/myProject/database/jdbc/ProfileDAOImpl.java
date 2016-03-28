2f9fad1c5bf477954a325879dc5d3c0c4fcc5ab5
Camille Makurin <makurincamille@inbox.lv>
2016-03-28 22:52:08 +0300

    updated ProfileDAOImpl class

--- src/main/java/myProject/database/jdbc/ProfileDAOImpl.java
+++ src/main/java/myProject/database/jdbc/ProfileDAOImpl.java
@@ -2,10 +2,13 @@ package myProject.database.jdbc;
 
 import myProject.database.DBException;
 import myProject.database.ProfileDAO;
-import sun.java2d.cmm.Profile;
+import myProject.domain.profile.Profile;
+
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by Camille on 28.03.2016.
@@ -21,11 +24,27 @@ public class ProfileDAOImpl extends DAOImpl implements ProfileDAO {
       try {
           connection = getConnection();
           PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO mytestdatabase.profile values (default,?,?,?,?,?,?,?)",PreparedStatement.RETURN_GENERATED_KEYS);
+          preparedStatement.setString(1,profile.getFirstName());
+          preparedStatement.setString(2,profile.getFamilyName());
+          preparedStatement.setString(3,profile.getBirthDate());
+          preparedStatement.setString(4,profile.getCharacterTrait1());
+          preparedStatement.setString(5,profile.getCharacterTrait2());
+          preparedStatement.setString(6,profile.getCharacterTrait3());
+          preparedStatement.setString(7,profile.getDescription());
+          preparedStatement.executeUpdate();
+          ResultSet resSet = preparedStatement.getGeneratedKeys();
+          if (resSet.next()){
+              profile.setProfileId(resSet.getLong(1));
+          }
 
 
 
-
-      }
+      }  catch (Throwable e) {
+          System.out.println("Exception while execute UserDAOImpl.create()");
+          e.printStackTrace();
+          throw new DBException(e);
+      } finally {
+          closeConnection(connection);}
   }
 
 }
